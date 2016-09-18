ef9fe9bb6b78c7ab4586e377b1a0826f34ac1d5d
sergio <sata@ic.ufal.br>
2016-09-18 20:39:34 -0300

    changes in professional profile now triggers a log to repository

--- iface-profile/src/main/java/br/ufal/ic/iface_profile/controller/profile/UserProfessionalProfileController.java
+++ iface-profile/src/main/java/br/ufal/ic/iface_profile/controller/profile/UserProfessionalProfileController.java
@@ -1,13 +1,32 @@
 package br.ufal.ic.iface_profile.controller.profile;
 
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.MediaType;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
 import br.ufal.ic.iface_profile.controller.AbstractController;
+import br.ufal.ic.iface_profile.exceptions.ValidationException;
+import br.ufal.ic.iface_profile.model.infrastructure.User;
 import br.ufal.ic.iface_profile.model.profile.UserProfessionalProfile;
+import br.ufal.ic.iface_profile.model.storytelling.UserLog;
+import br.ufal.ic.iface_profile.repository.classes.storytelling.UserLogRepository;
 import br.ufal.ic.iface_profile.repository.interfaces.profile.UserProfessionalProfileRepositoryInterface;
 
 @RestController
@@ -23,6 +42,83 @@ public class UserProfessionalProfileController extends AbstractController<UserPr
 		return repository;
 	}
 	
+	@RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+	@ResponseBody
+	public void delete(@PathVariable Integer id) {
+		UserProfessionalProfile deletedUserProfessionalProfile = getRepository().findById(id);
+		
+		User user = deletedUserProfessionalProfile.getUser();
+		
+		UserLog userLog = new UserLog();
+		userLog.setUser(user);
+		
+		userLog.setActivity("Remove professional profile");
+		
+		userLog.setTitle(user.getName()+
+							" removed "+
+								deletedUserProfessionalProfile.getOffice()+
+									" from profile");
+		
+		userLog.setTimestamp(new Date());
+		
+		UserLogRepository userLogRepository = new UserLogRepository();
+		userLogRepository.save(userLog);
+		
+		getRepository().delete(deletedUserProfessionalProfile);
+	}
 	
+	@RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public UserProfessionalProfile save(@RequestBody @Valid UserProfessionalProfile newUserProfessionalProfile, BindingResult result,
+			HttpServletResponse response) throws JsonParseException,
+			JsonMappingException, IOException {
+		if (result.hasErrors()) {
+			throw new ValidationException(result);
+		}
+		
+		UserLog userLog = new UserLog();		
+		User user = newUserProfessionalProfile.getUser();
+		userLog.setUser(user);
+		
+		userLog.setActivity("Add professional profile");
+		userLog.setTitle(user.getName()+" added "+
+							newUserProfessionalProfile.getOffice()+
+								" to profile");
+		
+		userLog.setTimestamp(new Date());
+		
+		UserLogRepository userLogRepository = new UserLogRepository();
+		userLogRepository.save(userLog);
+
+		return getRepository().save(newUserProfessionalProfile);
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public UserProfessionalProfile update(@RequestBody @Valid UserProfessionalProfile newUserProfessionalProfile, BindingResult result,
+			HttpServletResponse response) throws JsonParseException,
+			JsonMappingException, IOException {
+		if (result.hasErrors()) {
+			throw new ValidationException(result);
+		}
+		
+		UserLog userLog = new UserLog();		
+		User user = newUserProfessionalProfile.getUser();
+		userLog.setUser(user);
+		
+		userLog.setActivity("Professional profile modification");
+		
+		userLog.setTitle(user.getName()+
+							" modified "+
+								newUserProfessionalProfile.getOffice()+
+									" on profile");
+		
+		userLog.setTimestamp(new Date());
+		
+		UserLogRepository userLogRepository = new UserLogRepository();
+		userLogRepository.save(userLog);
+
+		return getRepository().update(newUserProfessionalProfile);
+	}
 
 }
