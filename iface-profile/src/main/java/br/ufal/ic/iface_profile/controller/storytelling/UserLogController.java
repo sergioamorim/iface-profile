eb73f75938f3751bc508e49b4a134247c23e0224
sergio <sata@ic.ufal.br>
2016-09-18 17:40:04 -0300

    removed unused imports

--- iface-profile/src/main/java/br/ufal/ic/iface_profile/controller/storytelling/UserLogController.java
+++ iface-profile/src/main/java/br/ufal/ic/iface_profile/controller/storytelling/UserLogController.java
@@ -1,28 +1,17 @@
 package br.ufal.ic.iface_profile.controller.storytelling;
 
-import java.io.IOException;
 import java.util.List;
-
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.MediaType;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-
 import br.ufal.ic.iface_profile.controller.AbstractController;
-import br.ufal.ic.iface_profile.exceptions.ValidationException;
 import br.ufal.ic.iface_profile.model.infrastructure.User;
 import br.ufal.ic.iface_profile.model.storytelling.UserLog;
 import br.ufal.ic.iface_profile.repository.interfaces.storytelling.UserLogRepositoryInterface;
