617d48f817fc8d5502ea87c9eea6d70f496bb890
sergio <sata@ic.ufal.br>
2016-09-18 18:45:38 -0300

    delete method now creates a UserLog and saves it to repository

--- iface-profile/src/main/java/br/ufal/ic/iface_profile/controller/friendship/FriendshipController.java
+++ iface-profile/src/main/java/br/ufal/ic/iface_profile/controller/friendship/FriendshipController.java
@@ -1,10 +1,12 @@
 package br.ufal.ic.iface_profile.controller.friendship;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 import br.ufal.ic.iface_profile.controller.AbstractController;
 import br.ufal.ic.iface_profile.model.friendship.Friendship;
 import br.ufal.ic.iface_profile.model.infrastructure.User;
+import br.ufal.ic.iface_profile.model.storytelling.UserLog;
+import br.ufal.ic.iface_profile.repository.classes.storytelling.UserLogRepository;
 import br.ufal.ic.iface_profile.repository.interfaces.friendship.FriendshipRepositoryInterface;
 
 @RestController
@@ -34,4 +38,34 @@ public class FriendshipController extends AbstractController <Friendship, Intege
     public List<Friendship> getFriends() {
 		return getRepository().findFriends(new User());
     }
+	
+	@RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+	@ResponseBody
+	public void delete(@PathVariable Integer id) {
+		Friendship deletedFriendship = getRepository().findById(id);
+		
+		User user_x = deletedFriendship.getUser_x();
+		User user_y = deletedFriendship.getUser_y();
+		
+		UserLog userXLog = new UserLog();
+		UserLog userYLog = new UserLog();
+		
+		userXLog.setUser(user_x);
+		userYLog.setUser(user_y);
+		
+		userXLog.setActivity("Desfazer amizade");
+		userYLog.setActivity("Desfazer amizade");
+		
+		userXLog.setTimestamp(new Date());
+		userYLog.setTimestamp(new Date());
+		
+		userXLog.setTitle("Desfez a amizade com"+user_y.getName());
+		userYLog.setTitle("Desfez a amizade com"+user_x.getName());
+		
+		UserLogRepository userLogRepository = new UserLogRepository();
+		userLogRepository.save(userXLog);
+		userLogRepository.save(userYLog);
+		
+		getRepository().delete(deletedFriendship);
+	}
 }
