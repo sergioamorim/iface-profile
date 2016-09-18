48033453271b7c8385c3bab62569bd45c3b9b631
sergio <sata@ic.ufal.br>
2016-09-18 20:30:18 -0300

    changes in user contact now triggers a user log to repository

--- iface-profile/src/main/java/br/ufal/ic/iface_profile/controller/profile/UserContactController.java
+++ iface-profile/src/main/java/br/ufal/ic/iface_profile/controller/profile/UserContactController.java
@@ -1,13 +1,32 @@
 package br.ufal.ic.iface_profile.controller.profile;
 
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.MediaType;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
 import br.ufal.ic.iface_profile.controller.AbstractController;
+import br.ufal.ic.iface_profile.exceptions.ValidationException;
+import br.ufal.ic.iface_profile.model.infrastructure.User;
 import br.ufal.ic.iface_profile.model.profile.UserContact;
+import br.ufal.ic.iface_profile.model.storytelling.UserLog;
+import br.ufal.ic.iface_profile.repository.classes.storytelling.UserLogRepository;
 import br.ufal.ic.iface_profile.repository.interfaces.profile.UserContactRepositoryInterface;
 
 @RestController
@@ -20,6 +39,85 @@ public class UserContactController extends AbstractController<UserContact, Integ
 
 	protected UserContactRepositoryInterface getRepository() {
 		return repository;
+	}
+	
+	@RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+	@ResponseBody
+	public void delete(@PathVariable Integer id) {
+		UserContact deletedUserContact = getRepository().findById(id);
+		
+		User user = deletedUserContact.getUser();
+		
+		UserLog userLog = new UserLog();
+		userLog.setUser(user);
+		
+		userLog.setActivity("Remove contact");
+		
+		userLog.setTitle(user.getName()+
+							" removed "+
+								deletedUserContact.getContact()+
+									" from profile");
+		
+		userLog.setTimestamp(new Date());
+		
+		UserLogRepository userLogRepository = new UserLogRepository();
+		userLogRepository.save(userLog);
+		
+		getRepository().delete(deletedUserContact);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public UserContact save(@RequestBody @Valid UserContact newUserContact, BindingResult result,
+			HttpServletResponse response) throws JsonParseException,
+			JsonMappingException, IOException {
+		if (result.hasErrors()) {
+			throw new ValidationException(result);
+		}
+		
+		UserLog userLog = new UserLog();		
+		User user = newUserContact.getUser();
+		userLog.setUser(user);
+		
+		userLog.setActivity("Add contact");
+		userLog.setTitle(user.getName()+" added "+
+							newUserContact.getContact()+
+								" to profile");
+		
+		userLog.setTimestamp(new Date());
+		
+		UserLogRepository userLogRepository = new UserLogRepository();
+		userLogRepository.save(userLog);
+
+		return getRepository().save(newUserContact);
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public UserContact update(@RequestBody @Valid UserContact newUserContact, BindingResult result,
+			HttpServletResponse response) throws JsonParseException,
+			JsonMappingException, IOException {
+		if (result.hasErrors()) {
+			throw new ValidationException(result);
+		}
+		
+		UserLog userLog = new UserLog();		
+		User user = newUserContact.getUser();
+		userLog.setUser(user);
+		
+		userLog.setActivity("Contact modification");
+		
+		userLog.setTitle(user.getName()+
+							" modified "+
+								newUserContact.getContact()+
+									" on profile");
+		
+		userLog.setTimestamp(new Date());
+		
+		UserLogRepository userLogRepository = new UserLogRepository();
+		userLogRepository.save(userLog);
+
+		return getRepository().update(newUserContact);
 	}
 	
 
