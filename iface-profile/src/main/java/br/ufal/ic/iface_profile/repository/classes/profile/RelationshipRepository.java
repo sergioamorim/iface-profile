13c006ae125b810722e2364a0e4f9ec11a75c506
Lucas Peixoto <lpeixoto80@gmail.com>
2016-09-24 17:46:45 -0300

    Some methods needed to implementation of the relationship structure.(1)

--- iface-profile/src/main/java/br/ufal/ic/iface_profile/repository/classes/profile/RelationshipRepository.java
+++ iface-profile/src/main/java/br/ufal/ic/iface_profile/repository/classes/profile/RelationshipRepository.java
@@ -1,7 +1,11 @@
 package br.ufal.ic.iface_profile.repository.classes.profile;
 
+import java.util.List;
+
+import org.hibernate.SQLQuery;
 import org.springframework.stereotype.Component;
 
+import br.ufal.ic.iface_profile.model.infrastructure.User;
 import br.ufal.ic.iface_profile.model.profile.Relationship;
 import br.ufal.ic.iface_profile.repository.classes.GenericHibernateRepository;
 import br.ufal.ic.iface_profile.repository.interfaces.profile.RelationshipRepositoryInterface;
@@ -9,5 +13,14 @@ import br.ufal.ic.iface_profile.repository.interfaces.profile.RelationshipReposi
 @Component
 public class RelationshipRepository extends GenericHibernateRepository<Relationship, Integer>
 				implements RelationshipRepositoryInterface{
+	
+	@SuppressWarnings("unchecked")
+	public List<User> findRelationships(Integer x){
+		SQLQuery q1 = this.getSession().createSQLQuery("SELECT * FROM relationship "
+				+ "WHERE relationship.sender_id=:x_id OR relationship.receiver_id=:x_id").addEntity(Relationship.class);
+		q1.setInteger("x_id", x);
+		return q1.list();
+	}
+
 
 }
--- iface-profile/src/main/java/br/ufal/ic/iface_profile/repository/interfaces/profile/RelationshipRepositoryInterface.java
+++ iface-profile/src/main/java/br/ufal/ic/iface_profile/repository/interfaces/profile/RelationshipRepositoryInterface.java
@@ -1,8 +1,12 @@
 package br.ufal.ic.iface_profile.repository.interfaces.profile;
 
+import java.util.List;
+
+import br.ufal.ic.iface_profile.model.infrastructure.User;
 import br.ufal.ic.iface_profile.model.profile.Relationship;
 import br.ufal.ic.iface_profile.repository.interfaces.RepositoryInterface;
 
 public interface RelationshipRepositoryInterface extends RepositoryInterface<Relationship, Integer>{
 
+	public List<User> findRelationships(Integer x);
 }
